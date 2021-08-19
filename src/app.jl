c37b5efa8bada5707982684fdc3934dcb8f710f1
Ashwani Rathee <ab669522@gmail.com>
2021-08-19 21:55:31 +0530

    Update

--- src/app.jl
+++ src/app.jl
@@ -1,5 +1,8 @@
 using Genie
 using Genie.Router
+using HTTP
+import Genie.Renderer.Json: json
+using Genie.Renderer.Json, Genie.Requests
 
 function launchServer(port)
 
@@ -10,10 +13,28 @@ function launchServer(port)
     println("port set to $(port)")
 
     route("/") do
-        "Hi there!"
+        "Hi there! This is server 1"
+    end
+    route("/jsontest") do
+        (:message => "Hi there!this is a json test") |> json
     end
 
-    Genie.AppServer.startup()
+    route("/echo", method = POST) do
+        message = jsonpayload()
+        (:echo => (message["message"] * " ")^message["repeat"]) |> json
+    end
+
+    route("/send") do
+        response = HTTP.request(
+            "POST",
+            "https://julia-apiserver2.herokuapp.com/echo",
+            [("Content-Type", "application/json")],
+            """{"message":"hello", "repeat":3}""",
+        )
+
+        response.body |> String |> json
+    end
+    Genie.AppServer.startup(async = false)
 end
 
 launchServer(parse(Int, ARGS[1]))
